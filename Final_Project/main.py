ed3e1a08733968804b30849e0d4cd4d47768184c
raiden-l <raidenlor@gmail.com>
2026-05-21 19:54:07 -0500

    Add files via upload
    Playable terminal-based game. Implemented scoring system based on dataset. Scoring system needs tweaking.

--- Final_Project/main.py
+++ Final_Project/main.py
@@ -1,16 +1,52 @@
 from models.campus_graph import CampusGraph
-
+from models.sustainability_game import SustainabilityGame
 
 def main():
     print("Loading campus graph...\n")
 
     graph = CampusGraph()
-
     graph.load_edges("data/ust_building_edges-1.csv")
 
-    graph.print_graph() # For checking accuracy; can be removed later.
+    # graph.print_graph() # For checking accuracy; can be removed later.
 
+    game = SustainabilityGame(graph)
+    
+    print("Available Buildings:")
 
+    for building in graph.graph.keys():
+        print("-", building)
+
+    choosing = True
+    while choosing:
+        start = input("\nChoose starting building: ").strip()
+
+        if start not in graph.graph.keys():
+            print("Invalid entry.")
+            continue
+        
+        choosing = False
+
+    game.start_game(start)
+
+    while True:
+
+        game.display_status()
+
+        if game.game_complete():
+
+            print(
+                "\nAll sustainability "
+                "sites collected!"
+            )
+            break
+
+        move = input(
+            "\nMove to building (or quit): ").strip()
+
+        if move.lower() == "quit":
+            break
+
+        game.move(move)
 
 if __name__ == "__main__":
     main()
